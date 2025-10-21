c996dc91f7c12e93fac71e33d9de98fe05dad5d3
AzR919 <azaanrehman919@gmail.com>
2025-10-20 23:06:36 -0700

    expected files

---
+++ inference/inputs.py
@@ -0,0 +1,17 @@
+"""
+File to prepare inputs for CANDI inference
+
+"""
+
+import os
+import sys
+from pathlib import Path
+
+Project_DIR = Path(__file__).parents[1].resolve()
+sys.path.insert(1, str(Project_DIR))
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
